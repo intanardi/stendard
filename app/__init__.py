9b0fdc5da913acab4c3a4432fb24d8c2a3c2c742
ardi <intan.ardipramanova@gmail.com>
2022-04-06 13:50:24 +0700

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
-from flask_cors import CORS, cross_origin
-from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 csrf = CSRFProtect()
-bootstrap = Bootstrap()
+
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    CORS(app, resources={
-        r"/*": {"origins": "*"}
-    })
-
-    db.init_app(app)
     csrf.init_app(app)
     bootstrap.init_app(app)
+    db.init_app(app)
+
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
