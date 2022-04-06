8d21512592055dd1c5686c051625c9ac2244d1e3
ardi <intan.ardipramanova@gmail.com>
2022-04-06 13:59:13 +0700

    config

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     # MONGO_URI = "mongodb://localhost:27017/example_data"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/stendard_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ardi:1234@localhost:3306/stendard_db'
 
 config = {
     'development' : DevelopmentConfig
